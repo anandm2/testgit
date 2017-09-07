e59f3a4facf0e7f92e2bd088f1a04e8cbb902c6f
anandm2 <anandm_2@yahoo.com>
2017-09-07 07:08:45 -0400

    tesng-firstproject
    testng-firstproject

---
+++ src/com/java/testng/automation/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.java.testng.automation;
+
+import org.testng.annotations.Test;
+
+
+public class HelloWorld {
+	
+	@Test
+	public void testng21()
+	{
+		System.out.println("first program! Hello world");
+	}
+
+}
---
+++ src/com/java/testng/automation/HelloWorld2.java
@@ -0,0 +1,13 @@
+package com.java.testng.automation;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld2 {
+	
+	@Test
+	public void testng21()
+	{
+		System.out.println("first program!");
+	}
+
+}
